3e8e4e6ee26522025c0ead869a5272bc8fa5fda9
Benny William Pardede <bpardedewilliam@gmail.com>
2018-03-23 22:37:00 +0700

    Mandatory checklist 4th

--- tutorial-6/src/main/java/Customer.java
+++ tutorial-6/src/main/java/Customer.java
@@ -28,21 +28,14 @@ class Customer {
 
         while (iterator.hasNext()) {
             Rental each = iterator.next();
-            double thisAmount = 0;
-
-            thisAmount = amountFor(each);
 
             // Add frequent renter points
-            frequentRenterPoints++;
-
-            // Add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // Show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         // Add footer lines
@@ -53,22 +46,6 @@ class Customer {
     }
 
     public double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return aRental.getCharge();
     }
 }
