427c0cf56272b2ae63cd978e1ebb7c2ded50e938
Benny William Pardede <bpardedewilliam@gmail.com>
2018-04-06 21:23:54 +0700

    Excercise 1 Checkpoint

--- tutorial-7/src/main/java/PrimeChecker.java
+++ tutorial-7/src/main/java/PrimeChecker.java
@@ -1,10 +1,12 @@
+import java.util.stream.IntStream;
+
 /**
  * 1st exercise.
  */
 public class PrimeChecker {
 
     public static boolean isPrime(int number) {
-        boolean divisible = false;
+        /*boolean divisible = false;
 
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
@@ -13,7 +15,21 @@ public class PrimeChecker {
             }
         }
 
-        return number > 1 && !divisible;
+        return number > 1 && !divisible;*/
+        /*
+        isPrime(1)? false
+        isPrime(2)? true
+        isPrime(3)? true
+        isPrime(4)? false
+        isPrime(5)? true
+        isPrime(6)? false
+        isPrime(7)? true
+        isPrime(8)? false
+        isPrime(9)? false
+        */
+        return number > 1 &&
+            IntStream.range(2, number)
+                     .noneMatch(i -> number % i == 0);
     }
 
     public static void main(String[] args) {
