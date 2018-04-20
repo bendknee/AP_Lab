eb49cc8c508f2e9cd5250dfe2c215f2e90ac9e1b
Benny William Pardede <bpardedewilliam@gmail.com>
2018-04-20 23:33:09 +0700

    Javari Controller and Javari Backend done

--- tutorial-9/src/main/java/tutorial/javari/JavariController.java
+++ tutorial-9/src/main/java/tutorial/javari/JavariController.java
@@ -1,5 +1,61 @@
 package tutorial.javari;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import tutorial.javari.animal.Animal;
+import tutorial.javari.exceptions.EmptyZooException;
+import tutorial.javari.exceptions.NoSuchID;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/javari")
 public class JavariController {
-    // TODO Implement me!
+
+    private JavariBackend model = new JavariBackend();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ArrayList<Animal> getAll() {
+        if (model.animalCount() > 0) {
+            return model.wholeZoo();
+        }
+        else {
+            throw new EmptyZooException();
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Animal getAnimal(@PathVariable("id") int id) {
+        if (model.animalCount() == 0) {
+            throw new EmptyZooException();
+        }
+        if (model.idExists(id)) {
+            return model.getAnimal(id);
+        }
+        else {
+            throw new NoSuchID();
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Animal removeAnimal(@PathVariable("id") int id) {
+        if (model.animalCount() == 0) {
+            throw new EmptyZooException();
+        }
+        if (model.idExists(id)) {
+            return model.removeAnimal(id);
+        }
+        else {
+            throw new NoSuchID();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Animal addAnimal(@RequestBody String JSONformat) {
+        return model.addAnimal(JSONformat);
+    }
+
 }
---
+++ tutorial-9/src/main/java/tutorial/javari/exceptions/NoSuchID.java
@@ -0,0 +1,9 @@
+package tutorial.javari.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No matching animal ID Found")
+public class NoSuchID extends RuntimeException {
+}
+
